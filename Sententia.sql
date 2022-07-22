decf4b93aaabceb8a5ca54cf75e101fec7a6da30
Username-Leon <leonardo.temperanza@gmail.com>
2022-07-22 21:37:29 +0200

    Mi ero dimenticato il "Attivo" in piano

--- Sententia.sql
+++ Sententia.sql
@@ -152,6 +152,7 @@ CREATE TABLE Piano(
 	CodUtentePremium varchar(55) NOT NULL, 
 	Quantita int NOT NULL, 
 	Periodo periodoPiano NOT NULL,
+	Attivo bool NOT NULL,
 	FOREIGN KEY (CodUtentePremium) REFERENCES UtentePremium(CodUtente),
 	FOREIGN KEY (CodR) REFERENCES Recensione(CodR)
 );
