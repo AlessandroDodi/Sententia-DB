8466a4e67ea5e06f296328a83fa029e5ffadc89f
Username-Leon <leonardo.temperanza@gmail.com>
2022-07-23 17:02:46 +0200

    Aggiunta primo trigger

--- Trigger e viste.sql
+++ Trigger e viste.sql
@@ -17,4 +17,46 @@ WHERE R1.CodR NOT IN (
 	  AND Rimozione.DAnnullamento IS NULL
 )
 
-Trigger
+Trigger (testare quando ci sono dei dati)
+
+////////////////////////////
+
+/* Il primo argomento è vero se si tenta di inserire una transazione manuale, falso altrimenti */
+CREATE FUNCTION TotalitaGerarchiaTrans() RETURNS TRIGGER AS $$
+DECLARE
+	Cond BOOL;
+BEGIN
+	IF OLD.TRN = NEW.TRN THEN RETURN NEW;
+	END IF;
+
+	IF TG_NARGS <> 1 THEN RAISE EXCEPTION 'Argomenti forniti erroneamente';
+	END IF;
+	
+	IF TG_ARGV[0] = TRUE
+	THEN
+		Cond = EXISTS(SELECT TRN
+			      FROM TransazioneAutomatica
+			      WHERE TRN = NEW.TRN);
+	ELSE
+		Cond = EXISTS(SELECT TRN
+			      FROM TransazioneManuale
+			      WHERE TRN = NEW.TRN);
+	END IF;
+	
+	IF Cond
+	THEN RAISE EXCEPTION 'Questo codice TRN è già presente nel database.';
+	END IF;
+	
+	RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER GestioneInserTransManuale
+AFTER INSERT OR UPDATE ON TransazioneManuale
+FOR EACH ROW EXECUTE PROCEDURE TotalitaGerarchiaTrans(TRUE);
+
+CREATE TRIGGER GestioneInserTransAuto
+AFTER INSERT OR UPDATE ON TransazioneAutomatica
+FOR EACH ROW EXECUTE PROCEDURE TotalitaGerarchiaTrans(FALSE);
+
+/////////////////////////////////////////
