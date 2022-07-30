7f2b1ef43246639f097f0399bd53802ef8c9b0f1
samanjot <singhamanjot726@gmail.com>
2022-07-30 09:46:02 +0200

    add new security features
    Signed-off-by: samanjot <singhamanjot726@gmail.com>

--- security.sql
+++ security.sql
@@ -1,5 +1,54 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+CREATE USER client WITH ENCRYPTED PASSWORD 'topolino2022';
+
+GRANT SELECT, INSERT, UPDATE ON utente to client;
+GRANT SELECT ON moderatore to client;
+
+GRANT SELECT(email, username, nome, cognome, ip, foto, psw), 
+      INSERT(email, username, nome, cognome, ip, foto, psw), 
+      UPDATE(email, nome, cognome, ip, foto) ON utente to client;
+
+
+CREATE FUNCTION delete_user(IN user VARCHAR(55), IN pswd VARCHAR(64)) RETURNS BOOLEAN SECURITY DEFINER AS $$
+        BEGIN
+            IF NOT (SELECT user_login(user, pswd)) = TRUE 
+            THEN
+                RETURN FALSE;
+            END IF;
+
+            DELETE FROM utente WHERE username = user;
+            RETURN TRUE;
+        END           
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION change_psw(IN user VARCHAR(55), IN new_pswd VARCHAR(64), IN old_pswd VARCHAR(64)) 
+    RETURNS BOOLEAN SECURITY DEFINER AS $$
+        BEGIN
+            IF NOT (SELECT user_login(user, old_pswd)) = TRUE 
+            THEN
+                RETURN FALSE;
+            END IF;
+
+            UPDATE utente SET psw = new_pswd WHERE psw = crypt(old_pswd, psw);
+            RETURN TRUE;
+        END           
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION moderatore_login(IN user VARCHAR(55), IN pswd VARCHAR(64)) RETURNS BOOLEAN AS $$
+    DECLARE
+        successful BOOLEAN
+    BEGIN
+
+        SELECT (psw = crypt(pswd, psw)) INTO successful
+        FROM moderatore
+        WHERE username = user;
+
+        RETURN successful;
+
+    END;
+$$ LANGUAGE 'plpgsql';
+
 CREATE FUNCTION user_login(IN user VARCHAR(55), IN pswd VARCHAR(64)) RETURNS BOOLEAN AS $$
     DECLARE
         successful BOOLEAN
@@ -32,4 +81,3 @@ FOR EACH ROW EXECUTE PROCEDURE GestionePassword();
 CREATE TRIGGER GestionePasswordModeratore
 BEFORE INSERT OR UPDATE ON moderatore
 FOR EACH ROW EXECUTE PROCEDURE GestionePassword();
-
