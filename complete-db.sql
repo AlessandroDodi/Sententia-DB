3b19903b20e0d10dcc38f9d44c55f7570d1af7cf
samanjot <singhamanjot726@gmail.com>
2022-07-31 09:04:35 +0200

    complete-db: few changes into inserts
    Signed-off-by: samanjot <singhamanjot726@gmail.com>

--- complete-db.sql
+++ complete-db.sql
@@ -449,8 +449,8 @@ FOR EACH ROW EXECUTE PROCEDURE VincoloIscrizione();
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE USER client WITH ENCRYPTED PASSWORD 'topolino2022';
-
 GRANT ALL ON ALL TABLES IN SCHEMA "public" TO client;
+
 GRANT SELECT, INSERT, UPDATE ON utente to client;
 GRANT SELECT ON moderatore to client;
 
@@ -592,9 +592,9 @@ INSERT INTO Recensione
   (CodR, Foto, DataPubblicazione, Titolo, Valore, Descrizione, CodUtente, CodO, DataVisionePubblica)
 VALUES
   ('1', 'images/reviews/lalla/1.png', '2021-04-01', 'Migliore film di sempre', '100', 'Semplicemente il miglior film di sempre', 'lalla', '1', '2021-05-01'),
-  ('2', 'images/reviews/jimmy/2.png', '2021-04-03', 'Poesia estremamente profonda', '100', 'Questa è la mia poesia preferita di Bukowski. La trovo molto profonda.', 'jimmy', '5', null),
-  ('3', 'images/reviews/jimmy/3.png', '2022-05-13', 'Miglior film di Robin Williams', '100', 'Film meraviglioso. A mio parare miglior prestazione di Robin Williams', 'sara', '2', null),
-  ('4', 'images/reviews/sara/4.png', '2022-05-14', 'Il mio podcast preferito', '95', 'Questo è il mio podcast preferito. Joe è molto intelligente e fa ottime domande. Mi piace molto la varietà nel tipo delle persone chiamate', 'jimmy', '4', null),
+  ('2', 'images/reviews/jimmy/2.png', '2021-04-03', 'Poesia estremamente profonda', '100', 'Questa è la mia poesia preferita di Bukowski. La trovo molto profonda.', 'jimmy', '5', '2021-04-03'),
+  ('3', 'images/reviews/sara/3.png', '2022-05-13', 'Miglior film di Robin Williams', '100', 'Film meraviglioso. A mio parare miglior prestazione di Robin Williams', 'sara', '2', '2022-05-14'),
+  ('4', 'images/reviews/jimmy/4.png', '2022-05-14', 'Il mio podcast preferito', '95', 'Questo è il mio podcast preferito. Joe è molto intelligente e fa ottime domande. Mi piace molto la varietà nel tipo delle persone chiamate', 'jimmy', '4', '2022-05-14'),
   ('5', 'images/reviews/marcus/5.png', '2022-05-15', 'Comprate sul mio sito', '90', 'Comprate bitcoin sul mio sito comprabitcoin.com', 'marcus', '6', null),
   ('6', 'images/reviews/sara/5.png', '2022-05-16', 'Imperdibile', '94', 'Sarò breve: questo film entra di diritto nel club dei film unici da riguardare più e più volte. la storia, almeno inizialmente, non appare imprevedibile e ricca di sorprese: Terra che sta diventando inabitabile, quindi serve un altro mondo dove abitare, quindi ecco il progetto dell viaggio interstellare. Ma ecco la rivoluzione: non ci si sofferma più nel mostrare le solite pompose scene tipiche dei film spaziali (piloti che si preparano, musiche eroiche, sfrenata dettagliatura delle navicelle spaziali), ma si vada dritti al cuore della narrazione. Alcuni dei vari accadimenti nello spazio non sono di immediata comprensione per lo spettatore, quasi una conseguenza logica vista la mole di nozioni tecniche su cui si basano: ma questo mi è piaciuto, attriubuisce una certo realismo e una logica che sfuggono allo spettatore medio, ma che vengono più che esaurientemente rese afferrabili col proseguire della trama.', 'sara', '1', '2022-10-01'),
   ('7', 'images/reviews/lalla/5.png', '2022-05-17', 'Stravolgente', '100','Questo film di psicologia ci mostra come non si debba mai dar per scontato un attore:robin williams,solitamente effervescenza e vitalità allo stato puro-l''attimo fuggente,mrs doubtfire,hook-qui è nei panni di uno psicologo,brillante e amante della vita ma non troppo al di sopra delle righe-e sia detto che considero l''attimo fuggente come uno dei film capolavoro della storia del cinema e keatting come il professore che avrei voluto avere-tuttavia il rientrare nelle righe di williams non mi dispiace affatto,ed lo s9i ritrova solo in risvegli con de niro-il cambiamento di williams è tale in questo flm che può essere paragonato a quello di fabrizi in roma città aperta, nel quale da comico si trasforma in un serio rappresentante di dio.', 'lalla', '2', null),
